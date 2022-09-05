a86eec3f381117346c8d766d4ea46f9726bdfc22
john <john.davis.2015.03@gmail.com>
2022-09-05 17:04:52 -0500

    adding header.css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,7 @@
-import React from "react";
 import "./header.css";
+import ME from "../../assets/me.png";
+import { CTA } from "./CTA";
+import { HeaderSocials } from "./HeaderSocials";
 
 export const Header = () => {
   return (
@@ -8,6 +10,16 @@ export const Header = () => {
         <h5>Hello I'm</h5>
         <h1>John Pablo</h1>
         <h5 className='text-light'>Fullstack Developer</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className='me'>
+          <img src={ME} alt='me' />
+        </div>
+
+        <a href='#contact' className='scroll__down'>
+          Scroll Down
+        </a>
       </div>
     </header>
   );
