3c9f1417820a0eca18fe030a439b6377a2641ab9
john <john.davis.2015.03@gmail.com>
2022-09-09 22:31:58 -0500

    add contact.css

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,85 @@
 import "./contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
+import { useRef } from "react";
+import emailjs from "emailjs-com";
 
 export const Contact = () => {
-  return <section id="contact">Contact</section>;
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      "service_q9138ym",
+      "template_243hxaw",
+      form.current,
+      "PLS1gaSu1LSbcHICa"
+    );
+    e.target.reset();
+  };
+
+  return (
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          <article className='contact__option'>
+            <MdOutlineEmail className='contact__option-icon' />
+            <h4>Email</h4>
+            <h5>john.davis.2015.03@gmail.com</h5>
+            <a
+              href='mailto:john.davis.2015.03@gmail.com'
+              target='_blank'
+              rel='noreferrer'>
+              Send a message
+            </a>
+          </article>
+          <article className='contact__option'>
+            <RiMessengerLine className='contact__option-icon' />
+            <h4>Messenger</h4>
+            <h5>John Davis</h5>
+            <a
+              href='https://m.me/profile.php?id=100009390640332'
+              target='_blank'
+              rel='noreferrer'>
+              Send a message
+            </a>
+          </article>
+          <article className='contact__option'>
+            <BsWhatsapp className='contact__option-icon' />
+            <h4>Whatsapp</h4>
+            <h5>0051 1 915944414</h5>
+            <a
+              href='https://wa.me/51915944414'
+              target='_blank'
+              rel='noreferrer'>
+              Send a message
+            </a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type='text'
+            name='name'
+            placeholder='Your Full Name'
+            required
+          />
+          <input type='email' name='email' placeholder='Your Email' required />
+          <textarea
+            name='message'
+            rows='7'
+            placeholder='Your Message'
+            required></textarea>
+          <button type='submit' className='btn btn-primary'>
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -4,58 +4,66 @@ import AVATAR3 from "../../assets/avatar3.jpg";
 import AVATAR4 from "../../assets/avatar4.jpg";
 import "./testimonials.css";
 
+// import Swiper core and required modules
+import { Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+const data = [
+  {
+    avatar: AVATAR1,
+    name: "Tina Snow",
+    review:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium odit fuga iure nemo dolorum! Doloremque deleniti alias vero voluptatibus, nam similique magni modi aliquam cumque facere est expedita suscipit illum",
+  },
+  {
+    avatar: AVATAR2,
+    name: "Shatta Wale",
+    review:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium odit fuga iure nemo dolorum! Doloremque deleniti alias vero voluptatibus, nam similique magni modi aliquam cumque facere est expedita suscipit illum",
+  },
+  {
+    avatar: AVATAR3,
+    name: "Kwame Despite",
+    review:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium odit fuga iure nemo dolorum! Doloremque deleniti alias vero voluptatibus, nam similique magni modi aliquam cumque facere est expedita suscipit illum",
+  },
+  {
+    avatar: AVATAR4,
+    name: "Nana Ama McBrown",
+    review:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium odit fuga iure nemo dolorum! Doloremque deleniti alias vero voluptatibus, nam similique magni modi aliquam cumque facere est expedita suscipit illum",
+  },
+];
+
 export const Testimonials = () => {
   return (
     <section id='testimonials'>
       <h5>Review from clients</h5>
       <h2>Testimonials</h2>
 
-      <div className='container testimonials__container'>
-        <article className='testimonial'>
-          <div className='client__avatar'>
-            <img src={AVATAR1} alt='Avatar One' />
-            <h5 className='client__name'>Ernest Achiever</h5>
-            <small className='client__review'>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima
-              dolorum quam maxime sunt libero deleniti ipsum distinctio aliquam
-              accusamus quasi.
-            </small>
-          </div>
-        </article>
-        <article className='testimonial'>
-          <div className='client__avatar'>
-            <img src={AVATAR1} alt='Avatar One' />
-            <h5 className='client__name'>Ernest Achiever</h5>
-            <small className='client__review'>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima
-              dolorum quam maxime sunt libero deleniti ipsum distinctio aliquam
-              accusamus quasi.
-            </small>
-          </div>
-        </article>
-        <article className='testimonial'>
-          <div className='client__avatar'>
-            <img src={AVATAR1} alt='Avatar One' />
-            <h5 className='client__name'>Ernest Achiever</h5>
-            <small className='client__review'>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima
-              dolorum quam maxime sunt libero deleniti ipsum distinctio aliquam
-              accusamus quasi.
-            </small>
-          </div>
-        </article>
-        <article className='testimonial'>
-          <div className='client__avatar'>
-            <img src={AVATAR1} alt='Avatar One' />
-            <h5 className='client__name'>Ernest Achiever</h5>
-            <small className='client__review'>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima
-              dolorum quam maxime sunt libero deleniti ipsum distinctio aliquam
-              accusamus quasi.
-            </small>
-          </div>
-        </article>
-      </div>
+      <Swiper
+        className='container testimonials__container'
+        // install Swiper modules
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}>
+        {data.map(({ avatar, name, review }, index) => {
+          return (
+            <SwiperSlide key={index} className='testimonial'>
+              <div className='client__avatar'>
+                <img src={avatar} alt={name} />
+              </div>
+              <h5 className='client__name'>{name}</h5>
+              <small className='client__review'>{review}</small>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </section>
   );
 };
